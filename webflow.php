b0eb149ee4287ed417e795fc91d6d539a7b56a8c
Morten Sordahl <morten@sordahl.dk>
2023-02-19 01:04:55 +0100

    init commit.

---
+++ webflow.php
@@ -0,0 +1,160 @@
+<?php
+namespace webflow;
+
+class webflow {
+    protected static string $url;
+    protected static string $site;
+    protected static array $contextOptions = ["ssl" => ["verify_peer" => false, "verify_peer_name" => false]];
+    private static int $maxDepth = 5;
+    private static array $links = [];
+    private static array $linksCrawled = [];
+
+    public function __construct($site)
+    {
+        self::$site = $site;
+        self::$url = 'https://' . $site;
+        print '-> Init scraping: ' . self::$url . PHP_EOL;
+        self::checkFolder('dist');
+        self::checkFolder('dist/assets');
+
+        $htmlRaw = self::getHtmlContent(self::$url);
+        $htmlRaw = self::cleanup_html($htmlRaw);
+        $htmlRaw = self::downloadExternalAssets($htmlRaw);
+        self::getLinks($htmlRaw);
+        $htmlRaw = str_replace(array("    " . PHP_EOL, PHP_EOL), "", $htmlRaw);
+
+        file_put_contents('dist/index.html', $htmlRaw);
+
+        self::recursiveParsePages();
+
+        print PHP_EOL;
+        print 'Completed; ' . (sizeof(self::$linksCrawled)+1) . ' pages crawled' . PHP_EOL;
+    }
+
+    public static function recursiveParsePages($depth=0): void
+    {
+        if ($depth >= self::$maxDepth) {
+            print '<- max depth reached' . PHP_EOL;
+            exit;
+        }
+        foreach(self::$links AS $page) {
+            if (!in_array($page, self::$linksCrawled)) {
+                print '-> Scraping: ' . $page . PHP_EOL;
+                $newPage = self::$url .'/' . $page;
+                $htmlRaw = self::getHtmlContent($newPage);
+                $htmlRaw = self::cleanup_html($htmlRaw);
+                $htmlRaw = self::downloadExternalAssets($htmlRaw);
+                $filename = self::renameLink($page);
+                self::getLinks($htmlRaw);
+                print '<- Saving file: ' . $filename . PHP_EOL;
+                file_put_contents('dist/' . $filename, $htmlRaw);
+                self::$linksCrawled[] = $page;
+
+                print '<- Scraping done (' . $page . ')' . PHP_EOL;
+            }
+        }
+
+        foreach(self::$links AS $link)
+            if (!in_array($link, self::$linksCrawled)) {
+                self::recursiveParsePages($depth++);
+                break;
+            }
+    }
+
+    public static function renameLink($link): string
+    {
+        return str_replace('/','_', $link) . '.html';
+    }
+
+    public static function getLinks(&$htmlRaw): void
+    {
+        $pattern = '(?:<a(?:.*?)href=\"\/)((?:[a-zA-Z])(?:.*?))(?:\"| )';
+        preg_match_all('/'.$pattern.'/', $htmlRaw, $linkList, PREG_PATTERN_ORDER);
+
+        foreach(array_unique(array_filter($linkList[1])) AS $link) {
+            if (!in_array($link, self::$links))
+                self::$links[] = $link;
+
+            $link = trim($link, '/');
+            //Rename links to local dist folder
+            //print '<- rename link: /' . $link . ' ->' . self::renameLink($link) . PHP_EOL;
+            $htmlRaw = str_replace('/' . $link .'"', self::renameLink($link) . '"', $htmlRaw);
+        }
+
+        $htmlRaw = str_replace('href="/"','href="./index.html"', $htmlRaw);
+    }
+
+    private static function checkFolder($folder): void
+    {
+        $path = realpath($folder);
+        print '<- Check folder: ' . $path . '... ';
+        if ($path === false || !is_dir($path))
+            mkdir($folder);
+
+        print (($path === false || !is_dir($path)) ? 'ERROR' : 'OK') . PHP_EOL;
+    }
+
+    public static function getHtmlContent($url): bool|string
+    {
+        return file_get_contents($url, false, stream_context_create(self::$contextOptions));
+    }
+
+    public static function cleanup_html($htmlRaw): array|string|null
+    {
+        $patterns = array(
+            '/<!--(.|\s)*?-->/', //Remove comments
+            '/(<meta content="Webflow" name="generator"\/>)/',
+            '/\?site=([a-zA-Z0-9]+)/', //Remove ?site= param from script src
+            //'/<script type(.|\s)*?<\/script>/', //Remove javascript
+            //'/<style>(.)*\<\/style>/', //Remove styles
+            //'/ data\-wf\-(domain|page|site|status|ignore)="(.)*?"/', //Remove webflow data tags
+            //'/<script src=\"https:\/\/uploads-ssl.webflow.com\/(.)*(js\/webflow)(.)*\"><\/script>/', //Remove webflow javascript library.
+
+        );
+        foreach ($patterns as $pattern)
+            $htmlRaw = preg_replace($pattern, '', $htmlRaw);
+
+        //Remove badge
+        $htmlRaw = str_replace('data-wf-domain="'.self::$site.'"','data-wf-domain="smalltasty.dk"',$htmlRaw);
+        //$htmlRaw = preg_replace('/<a class=\"w-webflow-badge\"(.*)<\/a>/', '', $htmlRaw);
+
+        return $htmlRaw;
+    }
+
+    public static function downloadExternalAssets($htmlRaw)
+    {
+        $files = [];
+        $pattern = '(https:\/\/(?:uploads-ssl.webflow.com)\/(?:.*?))(?:\"| )';
+        //$pattern = '(https:\/\/(?:.*?))(?:\"| )';
+        //$pattern = '(https:\/\/(?:.*?)\/(?:.*?)\.(?:.*?))(?:\"| )';
+        preg_match_all('/'.$pattern.'/', $htmlRaw, $fileList, PREG_PATTERN_ORDER);
+
+        //Clean-up fileList in to $files array.
+        foreach ($fileList[1] as $file)
+            if (!str_contains($file, ','))
+                $files[] = str_replace(['"','&quot;)'], '', $file);
+
+        if (empty($files))
+            return false;
+
+        foreach ($files as $file) {
+            $filename = explode('/', $file);
+            $filename = str_replace(array(' ','%20','(',')','webflow', 'sordahl'), '', end($filename));
+            $filename = trim($filename, '. ');
+
+
+            if (!file_exists('dist/assets/' . $filename)) {
+                print '<- Downloading: ' . $file . ' -> dist/assets/' . $filename;
+                $fileContent = file_get_contents($file, false, stream_context_create(self::$contextOptions));
+                if (in_array('Content-Encoding: gzip', $http_response_header)) $fileContent = gzdecode($fileContent);
+                file_put_contents('dist/assets/' . $filename, $fileContent);
+                print ' OK' . PHP_EOL;
+            }
+
+            $htmlRaw = str_replace($file, './assets/' . $filename, $htmlRaw);
+        }
+
+        return $htmlRaw;
+    }
+
+}
