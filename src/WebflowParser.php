cd91a5deeb586d5921a9b1089004d3e4da0db98b
sordahl <morten@sordahl.dk>
2025-08-19 10:09:01 +0200

    fix ascii replacements in filename

--- src/WebflowParser.php
+++ src/WebflowParser.php
@@ -17,15 +17,122 @@ class WebflowParser
 	public static $extension = '.html';
 	public static $dist = 'dist';
 	public static $appendHTML = '';
-	public static $removeCharacters = ['Æ', 'æ', 'ø', 'Ø', 'å', 'Å', ' ', '%20', '%40', '(', ')', 'webflow', 'sordahl'];
+	public static $removeFilenameCharacters = [
+		'Æ',
+		'æ',
+		'ø',
+		'Ø',
+		'å',
+		'Å',
+		' ',
+		'(',
+		')',
+		'webflow',
+		'sordahl',
+		'%20',
+		'%21',
+		'%22',
+		'%23',
+		'%24',
+		'%25',
+		'%26',
+		'%27',
+		'%28',
+		'%29',
+		'%2A',
+		'%2B',
+		'%2C',
+		'%2D',
+		'%2E',
+		'%2F',
+		'%30',
+		'%31',
+		'%32',
+		'%33',
+		'%34',
+		'%35',
+		'%36',
+		'%37',
+		'%38',
+		'%39',
+		'%3A',
+		'%3B',
+		'%3C',
+		'%3D',
+		'%3E',
+		'%3F',
+		'%40',
+		'%41',
+		'%42',
+		'%43',
+		'%44',
+		'%45',
+		'%46',
+		'%47',
+		'%48',
+		'%49',
+		'%4A',
+		'%4B',
+		'%4C',
+		'%4D',
+		'%4E',
+		'%4F',
+		'%50',
+		'%51',
+		'%52',
+		'%53',
+		'%54',
+		'%55',
+		'%56',
+		'%57',
+		'%58',
+		'%59',
+		'%5A',
+		'%5B',
+		'%5C',
+		'%5D',
+		'%5E',
+		'%5F',
+		'%60',
+		'%61',
+		'%62',
+		'%63',
+		'%64',
+		'%65',
+		'%66',
+		'%67',
+		'%68',
+		'%69',
+		'%6A',
+		'%6B',
+		'%6C',
+		'%6D',
+		'%6E',
+		'%6F',
+		'%70',
+		'%71',
+		'%72',
+		'%73',
+		'%74',
+		'%75',
+		'%76',
+		'%77',
+		'%78',
+		'%79',
+		'%7A',
+		'%7B',
+		'%7C',
+		'%7D',
+		'%7E'
+	];
 
 	public static function getAbsolutePath()
 	{
-		$scriptname = $_SERVER['SCRIPT_FILENAME'];
-		if (is_link($scriptname))
-			$scriptname = readlink($scriptname);
+		$scriptName = $_SERVER['SCRIPT_FILENAME'];
+		if (is_link($scriptName))
+			$scriptName = readlink($scriptName);
 
-		return dirname(realpath($scriptname));
+		return dirname(realpath($scriptName));
 	}
 
 	public function __construct(string $site, string $host)
@@ -118,7 +225,7 @@ class WebflowParser
 
 		foreach ($files as $file) {
 			$filename = explode('/', $file);
-			$filename = str_replace(self::$removeCharacters, '', end($filename));
+			$filename = str_replace(self::$removeFilenameCharacters, '', end($filename));
 			$filename = trim($filename, '. ');
 
 			if (!file_exists(self::$dist . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . $filename)) {
